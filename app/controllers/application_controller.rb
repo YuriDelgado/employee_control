f88f9709e3c494d778989df142be13014a902921
Yuri Delgado <yuri.m.delgado@gmail.com>
2019-10-15 10:47:42 -0500

    authorizing users_controller and fixing flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_mailer_host, :authenticate_user!
 
+  add_flash_types :danger, :info, :warning, :success
 
 protected
   def configure_permitted_parameters
